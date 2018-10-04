3dd44b4a43d3b69c29a67d7567789bd719ca8b94
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-10-04 05:29:27 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
-
-
+class Course
+  attr_accessor :title, :schedule, :description
+  
+  @@all = []
+  
+  def initiallize
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+end
